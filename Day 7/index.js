8ed5eaacaba0af648b0631c917025f9b624902ba
DT-Apple-Dot <114006230+DT-Apple-Dot@users.noreply.github.com>
2022-10-05 00:43:57 +0630

    #Day 7(Done)

---
+++ Day 7/index.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var app = express();
+
+require('dotenv').config()
+
+var test = require('./controllers/test')
+
+app.use('/test', test);
+app.listen(process.env.PORT, () => (console.log("Server Running")));
