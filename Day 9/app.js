3ee8e56dc927059a633eabf7fbce01dfa24cd24f
DT-Apple-Dot <mr.appledot.ygn@gmail.com>
2022-10-07 21:39:30 +0630

    #Day 9(Done)

---
+++ Day 9/app.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var app = express();
+
+app.set('view engine', 'pug');
+app.set('views','./views')
+
+app.get('/home', function(req, res){
+   res.render('index', {
+      name: "TutorialsPoint", 
+      url:"http://www.tutorialspoint.com"
+   });
+});
+
+app.listen(3000, () => {console.log("Server is Running on port 3000")});
